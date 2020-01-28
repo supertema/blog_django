e3fff951560dab9c2dbfe045f00bde0a34308357
supertema <supertema90@mail.ru>
2020-01-28 21:03:33 +0300

    add tags

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -4,12 +4,24 @@ from django.shortcuts import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True) # урлы, чпу
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts') # связь многое к многим для связи с тегами
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
-        return reverse('post_detail_url', kwargs={'slug': self.slug})
+        return reverse('post_detail_url', kwargs={'slug': self.slug}) # функция подтягивает ссылки на посты в шаблон
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
